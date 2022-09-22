fa4853fcd75c29801763b0c129cf038bd025b499
Donovan <donniemassenburg75@gmail.com>
2022-09-22 16:11:45 -0400

    linked the login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
 
 const Login = () => {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
     return  (
         <section className="login-sec">
         <div id className="selection-div login-window">
@@ -21,14 +57,19 @@ const Login = () => {
                 </div>
                 <div className="login-box-body">
                   {/* Login form */}
-                  <form className="login-form">
+                  <form className="login-form" onSubmit={handleFormSubmit}>
                     <div className="form-group">
                       <label htmlFor="login-email">&gt; Email address</label>
-                      <input type="email" className="form-control" id="login-email" aria-describedby="emailHelp" />
+                      <input type="email"
+                       className="form-control"
+                        id="login-email"
+                        aria-describedby="emailHelp"
+                        value={formState.email}
+                        onChange={handleChange}/>
                     </div>
                     <div className="form-group">
                       <label htmlFor="login-password">&gt; Password</label>
-                      <input type="password" className="form-control" id="login-password" />
+                      <input type="password" className="form-control" id="login-password" value={formState.password} onChange={handleChange}/>
                     </div>
                     <div className="form-group form-check">
                       <input type="checkbox" className="form-check-input" id="exampleCheck1" />
